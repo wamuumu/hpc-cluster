b11a872e11aad2d5fda90da1c39893b7c9ba0e32
Costalonga <matteo.costalonga@hpc-head-n1.unitn.it>
2024-10-10 12:26:24 +0200

    Added test3 parallel job

---
+++ test2-parallel-job/test2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#PBS -l select=1:ncpus=4:mem=2gb
+# set max execution time
+#PBS -l walltime=0:05:00
+# imposta la coda di esecuzione
+#PBS -q short_cpuQ
+module load mpich-3.2
+mpirun.actual -n 4 ../mpi-hello-world/mpi-hello
